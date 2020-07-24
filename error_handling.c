cf363c27629027a826b02c1a30b88cb862fee161
Roanoke <rowingwolfe@gmail.com>
2020-07-24 17:07:24 -0400

    Error handling 2.0 and interprocess communication

--- error_handling.c
+++ error_handling.c
@@ -4,16 +4,32 @@
 */
 
 #include <stdio.h>
-#include <string.h>
-#include <errno.h>
+#include <string.h> //strerror() lives in here, converts error number to a string explaining the error.
+#include <errno.h> //Error numbers
 #include <unistd.h>
+#include <stdlib.h> //Need exit()
+
+//There has to be a better way...
+//Oh wait.
+//Error handler.
+void error(char *msg){
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(-1);
+}
 
 int main (){
-
+    //If this fails (==-1) Otherwise run it.
     if(execl("/sbin/ifconfig", "/sbin/ifconfig", NULL) ==-1)
+        //Likewise to above.
         if(execlp("ipconfig", "ipconfig", NULL) ==-1){
-            fprintf(stderr, "Cannot run ipconfig: %s", strerror(errno));
-            return 1;
+
+            //Too much code.
+            //Print "Cannot run ipconfig and a string representation of the error."
+            //fprintf(stderr, "Cannot run ipconfig: %s", strerror(errno));
+            //Return -1 because shit went badly.
+            //return -1;
+
+            error("Cannot run ipconfig");
         }
 
     return 0;
