7171b3a874fcb63a972640234321d730797749fc
Roanoke <rowingwolfe@gmail.com>
2020-09-01 14:06:21 -0400

    Example of virtual functions added to C++ projects.

---
+++ C++ Projects/virtual_funcs.cpp
@@ -0,0 +1,94 @@
+/* 
+
+    An attempt at understanding virtual funcs 
+
+*/
+
+//I'll need some IO
+#include <iostream> //NOT stdio.h, we ain't C here.
+#include <string> //Also need strings, rather not use C style strings for simple shit.
+
+//Now a base class
+class Base{
+    private:
+    int m_arbitrary_private_number{5};
+
+    public:
+    std::string m_class_name{"Base"};
+
+    //Now a virtual function that can be overriden by derived versions based on type, etc.
+    virtual std::string print_class_name() const { return "I am Base \n"; }
+};
+
+
+//Now a derived class inheriting publicly(As is the most common form of inheritance.)
+class Derived : public Base{
+    private:
+    int m_derived_arbitrary_private_number{3}; //Nice and long. Perhaps too verbose, even.
+
+    public:
+    std::string m_class_name{"Derived"};
+
+    //Now an override for the virtual function
+    std::string print_class_name() const { return "I am Derived \n"; }
+};
+
+//Once more for effect!
+class Derived_Again : public Base{
+    private:
+    int really_not_important_at_all{2};
+
+    public:
+    std::string m_class_name{"Derived_Again"};
+
+    //Another overload!
+    std::string print_class_name() const { return "I am Derived_Again \n"; }
+};
+
+int main(){
+    //First an instance of Base
+    Base base;
+    //Now we instance the derived classes.
+    Derived d;
+    Derived_Again d_again;
+    //Now we make references of type Base to our derived classes.
+    Base &base_d{d};
+    Base &base_d_again{d_again};
+
+    //Print the base class' name
+    std::cout << base.print_class_name(); //I am Base
+
+    //Next the  derived class name from the base reference.
+    std::cout << base_d.print_class_name(); //I am Derived
+    //and finally the derived again
+    std::cout << base_d_again.print_class_name();//I am Derived_Again
+
+    //Then try to print the m_class_name from each.
+    std::cout << base.m_class_name << std::endl; //Base
+    std::cout << base_d.m_class_name << std::endl; //Base
+    std::cout << base_d_again.m_class_name << std::endl; //Base
+
+    //And from the actual instances of each class (Obj)
+    std::cout << base.m_class_name <<std::endl; //Base
+    std::cout << d.m_class_name <<std::endl; //Derived
+    std::cout << d_again.m_class_name <<std::endl; //Derived_Again
+
+    std::cout << "\n\nAnd now for the loop... \n\n";
+
+    //Where this might come in handy, say is something like this...
+    ///We have an array of Base refs to derived objects...
+    Base *my_base_ref_arr[] {&d, &d_again};
+    //Because Arrays have to be of ONE type. There are ways around this but... hacky-er than this from what I understand.
+    //Now we can loop through the array and call the overloaded funcs.
+    for(int i{0}; i < 2; i++){
+        std::cout << "-------------------------------------------- \n";
+        std::cout << my_base_ref_arr[i]->m_class_name << std::endl; //Base
+        //Now this will call the correct overriden function depending on the type
+        //And all of it can be contained in an iterable despite them being different types.
+        //Which I am certain is useful in ways I have not really considered as of yet since my brain is tired.
+        std::cout << my_base_ref_arr[i]->print_class_name() << std::endl;
+    }
+
+
+    return 0;
+}
